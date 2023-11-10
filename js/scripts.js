bb1f28285fdb1fbec019f8c6935b615cfe58619c
h-nguyen0417 <146160447+h-nguyen0417@users.noreply.github.com>
2023-11-10 16:42:08 -0700

    1.2
    Exercise 1.2: make pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World');
-var favoriteFood = 'Steak';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList =[
+    {name: 'Blastoise', height: 1.6, type: ['water']},
+    {name: 'Dugtrio', height: 0.7, type: ['ground']},
+    {name: 'Pidgeot', height: 1.5, type: ['flying','normal']}
+]
