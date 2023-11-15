f4ccf1b9d80f9ed12ba1dc43f3b863f1f48e1da3
h-nguyen0417 <146160447+h-nguyen0417@users.noreply.github.com>
2023-11-15 10:45:31 -0700

    1.3
    update for task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 let pokemonList = [];
 pokemonList =[
-    {name: 'Blastoise', height: 1.6, type: ['water']},//pokemon1 pokemon[0]
-    {name: 'Dugtrio', height: 0.7, type: ['ground']},//pokemon2 pokemon[1]
-    {name: 'Pidgeot', height: 1.5, type: ['flying','normal']}//pokemon3 pokemon[2]
+    {name: 'Blastoise', height: 1.6, type: ['water']},//pokemon1 pokemonList[0]
+    {name: 'Dugtrio', height: 0.7, type: ['ground']},//pokemon2 pokemonList[1]
+    {name: 'Pidgeot', height: 1.5, type: ['flying','normal']}//pokemon3 pokemonList[2]
 ]
 
 for(let i = 0; i <= pokemonList.length; i++){
-    document.write(pokemonList[i].name + '(' + pokemonList[i].height + ')')
+    if (pokemonList[i].height >= 1) {
+        document.write(pokemonList[i].name + ' (' + pokemonList[i].height + ')' + " - Sheesh! What a big Pokemon!" )
+    }
+    else{
+        document.write(pokemonList[i].name + ' (' + pokemonList[i].height + ')')
+    }
 }
