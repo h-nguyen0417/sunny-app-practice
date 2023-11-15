e6670068906b069d3c29bfd7e4e2040329479428
h-nguyen0417 <146160447+h-nguyen0417@users.noreply.github.com>
2023-11-15 09:42:17 -0700

    1.3
    task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 let pokemonList = [];
 pokemonList =[
-    {name: 'Blastoise', height: 1.6, type: ['water']},
-    {name: 'Dugtrio', height: 0.7, type: ['ground']},
-    {name: 'Pidgeot', height: 1.5, type: ['flying','normal']}
+    {name: 'Blastoise', height: 1.6, type: ['water']},//pokemon1 pokemon[0]
+    {name: 'Dugtrio', height: 0.7, type: ['ground']},//pokemon2 pokemon[1]
+    {name: 'Pidgeot', height: 1.5, type: ['flying','normal']}//pokemon3 pokemon[2]
 ]
+
+for(let i = 0; i <= pokemonList.length; i++){
+    document.write(pokemonList[i].name + '(' + pokemonList[i].height + ')')
+}
