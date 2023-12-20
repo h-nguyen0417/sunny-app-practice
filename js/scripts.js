607e3ed7d21d0276dee322759ab29ce34917e98f
h-nguyen0417 <146160447+h-nguyen0417@users.noreply.github.com>
2023-12-20 11:56:55 -0700

    Exercise 1.5
    Boom bb

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,39 @@
+let pokemonRepository = (function  () {
 
-let pokemonList =[
+let pokemonList =
+[
     {name: 'Blastoise', height: 1.6, type: ['water']},//pokemon1 pokemonList[0]
     {name: 'Dugtrio', height: 0.7, type: ['ground']},//pokemon2 pokemonList[1]
     {name: 'Pidgeot', height: 1.5, type: ['flying','normal']}//pokemon3 pokemonList[2]
 ]
+
+function getAll (){
+    return pokemonList;
+}
+
+function add(pokemon){
+        pokemonList.push(pokemon)    
+}
+
+return{
+    getAll: getAll,
+    add: add
+};
+
+})()
+
+pokemonRepository.getAll().forEach(printPokemon);
+
+function printPokemon(item){
+    document.write(item.name + ' (' + item.height + ') ' + item.type + ' ')
+    console.log(item.name + ' (' + item.height + ') ' + item.type)
+
+};
+
+
+//  pokemonList.forEach(function(user){
+//     document.write(user.name + user.height + user.type);
+//  });
 
 // for(let i = 0; i <= pokemonList.length; i++){
 //     if (pokemonList[i].height >= 1) {
@@ -13,16 +43,3 @@ let pokemonList =[
 //         document.write(pokemonList[i].name + ' (' + pokemonList[i].height + ')')
 //     }
 // } 
-
-pokemonList.forEach(printPokemon);
-function printPokemon(item){
-    document.write(item.name + ' (' + item.height + ')' + item.type)
-    console.log(item.name + ' (' + item.height + ')' + item.type)
-};
-
-
-//  pokemonList.forEach(function(user){
-//     document.write(user.name + user.height + user.type);
-//  });
-
-
