498e4da4a5221c75a6942fa0caa1a53778f133e8
h-nguyen0417 <146160447+h-nguyen0417@users.noreply.github.com>
2024-01-12 14:26:06 -0700

    cleanup
    reformatting and cleaning up to make it look pretty <3

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,22 @@
-let pokemonRepository = (function  () {
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Blastoise", height: 1.6, type: ["water"] }, //pokemon1 pokemonList[0]
+    { name: "Dugtrio", height: 0.7, type: ["ground"] }, //pokemon2 pokemonList[1]
+    { name: "Pidgeot", height: 1.5, type: ["flying", "normal"] }, //pokemon3 pokemonList[2]
+    { name: "Charzard", height: 1.2, type: ["fire", "flying"] },
+  ];
 
-let pokemonList =
-[
-    {name: 'Blastoise', height: 1.6, type: ['water']},//pokemon1 pokemonList[0]
-    {name: 'Dugtrio', height: 0.7, type: ['ground']},//pokemon2 pokemonList[1]
-    {name: 'Pidgeot', height: 1.5, type: ['flying','normal']},//pokemon3 pokemonList[2]
-    {name: 'Charzard', height: 1.2, type: ['fire', 'flying']}
-]
-
-function getAll (){
+  function getAll() {
     return pokemonList;
-}
+  }
 
-function add(pokemon){
-        pokemonList.push(pokemon)    
-}
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-function addListItem(pokemon){
-    let pokemonList = document.querySelector(".pokemon-list");//selects pokemonlist and links with pokemon-list in index.html
-    let listpokemon = document.createElement("li");//creates  a new li 
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list"); //selects pokemonlist and links with pokemon-list in index.html
+    let listpokemon = document.createElement("li"); //creates  a new li
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     button.classList.add("button-class");
@@ -27,48 +25,33 @@ function addListItem(pokemon){
     //attaching listpokemon to pokemonlist
     pokemonList.appendChild(listpokemon);
 
-    button.addEventListener('click', function(event){
-        showDetails(pokemon)
-    })
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
+  }
 
-}
+  function showDetails(pokemon) {
+    console.log(pokemon.name + pokemon.height + pokemon.type);
+    let createInfo = document.createElement("p");
+  }
 
-function showDetails(pokemon){
-    console.log(pokemon.name + pokemon.height + pokemon.type)
-    let createInfo = document.createElement('p')
-}
-
-
-
-
-
-
-
-
-return{
+  return {
     getAll: getAll,
     add: add,
     addListItem: addListItem,
-    showDetails: showDetails
-};
-
+    showDetails: showDetails,
+  };
 })();
 
 console.log(pokemonRepository.getAll());
-pokemonRepository.getAll().forEach(function (pokemon){
-pokemonRepository.addListItem(pokemon);
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
-
-
-
-
-
 
 // //adding a title variable
 // let mainTitle = document.querySelector('h1');// to see if selected do : console.log(mainTitle)
 // mainTitle.innerText = "helloworld";
 // console.log(mainTitle);
-
 
 // //adding a container varibale
 // let container = document.querySelector('.container')//without the dot, it will not be able to find the container. when we say the dot, it says its a class, and searches for that class.
@@ -99,4 +82,4 @@ pokemonRepository.addListItem(pokemon);
 //     else{
 //         document.write(pokemonList[i].name + ' (' + pokemonList[i].height + ')')
 //     }
-// } 
+// }
